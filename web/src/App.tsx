f210d538d04ed3de9a91453395677147badaac11
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-12-08 15:34:51 -0300

    componentes CreateButton e DeleteButton

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,12 @@
-
+import { CreateButton, DeleteButton } from "./components/Button";
 
 function App() {
-
   return (
-    <div>
-      <h1>Títulos</h1>
-      <h2>Subtítulos</h2>
-      <p>Texto</p>
+    <div className="flex gap-4 items-center justify-center min-h-screen">
+      <DeleteButton />
+      <CreateButton />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ web/src/components/Button.tsx
@@ -0,0 +1,22 @@
+import { FaPlusCircle } from "react-icons/fa";
+import { HiOutlineTrash} from 'react-icons/hi2'
+
+export function CreateButton() {
+  return (
+    <button
+      type="submit"
+      className="flex items-center p-4 gap-2 rounded-lg bg-dark-blue hover:bg-blue font-bold text-sm transition-all"
+    >
+      Criar
+      <FaPlusCircle />
+    </button>
+  );
+}
+
+export function DeleteButton() {
+  return (
+    <button type="submit" className="p-4 rounded-lg hover:bg-gray-400 text-gray-300 hover:text-danger">
+      <HiOutlineTrash size={20}/>
+    </button>
+  );
+}
