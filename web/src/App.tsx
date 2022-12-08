c40a8d99973bc0b0b24a7a6b7ace47a73a918c73
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-12-08 18:51:25 -0300

    componente input

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
 import { CreateButton, DeleteButton } from "./components/Button";
-import { InputCheckBox } from "./components/Input";
+import { InputCheckBox, InputText } from "./components/Input";
 
 function App() {
   return (
     <div className="flex gap-4 items-center justify-center min-h-screen">
-      <InputCheckBox />
+      <InputText placeholder="Adicione uma nova tarefa" />
     </div>
   );
 }
--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -2,14 +2,26 @@ import { BiCheck } from "react-icons/all";
 
 import * as Checkbox from "@radix-ui/react-checkbox";
 
-export function InputText() {}
+interface InputTextProps {
+  placeholder: string;
+}
+
+export function InputText({placeholder, ...props }:InputTextProps) {
+  return (
+    <label>
+      <input
+        className="w-full max-w-[32rem] rounded text-base p-4 bg-gray-500 placeholder:text-gray-300 focus:outline-none"
+        placeholder={placeholder}
+        {...props}
+      />
+    </label>
+  );
+}
 
 export function InputCheckBox() {
   return (
-    <label className="">
-      <Checkbox.Root
-        className="w-6 h-6 rounded-full border-2 border-blue hover:bg-dark-blue/20 hover:border-dark-blue checked:bg-dark-purple checked:hover:bg-purple checked:border-dark-purple checked:hover:border-purple"
-      >
+    <label>
+      <Checkbox.Root className="w-6 h-6 rounded-full border-2 border-blue hover:bg-dark-blue/20 hover:border-dark-blue checked:bg-dark-purple checked:hover:bg-purple checked:border-dark-purple checked:hover:border-purple">
         <Checkbox.Indicator>
           <BiCheck size={20} />
         </Checkbox.Indicator>
