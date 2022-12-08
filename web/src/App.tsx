92f05163a7c97b1897137359a0d845f7138c7548
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-12-08 15:03:09 -0300

    configurações iniciais

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>Títulos</h1>
+      <h2>Subtítulos</h2>
+      <p>Texto</p>
+    </div>
+  )
+}
+
+export default App
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          700: '#0D0D0D',
+          600: '#1A1A1A',
+          500: '#262626',
+          400: '#333333',
+          300: '#808080',
+          200: '#D9D9D9',
+          100: '#F2F2F2',
+        },
+        "blue": "#4EA8DE",
+        "dark-blue": "#1E6F9F",
+        "purple": "#8204FA",
+        "dark-purple": "#5E60CE",
+        'danger': '#E25858',
+      }
+    },
+  },
+  plugins: [],
+}
