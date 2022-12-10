d4bcb2f7eff07a76bf383bf2c4e0994f5972a5bc
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-12-09 23:06:22 -0300

    propriedades do tasklist

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,16 +2,17 @@ import { EmptyTaskList } from "./components/EmptyTaskList";
 import { TaskList } from "./components/TaskList";
 
 import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const [] = useState();
+
   return (
     <div className="flex flex-col gap-4 items-center justify-center">
       <Header />
 
       <main className="w-full max-w-[46rem] flex flex-col gap-16 p-4 sm:p-0">
-        
-
-        <TaskList />
+        <TaskList concludedTasks={2} tasksCreated={5} />
       </main>
     </div>
   );
--- web/src/components/TaskList.tsx
+++ web/src/components/TaskList.tsx
@@ -1,20 +1,26 @@
+import { useState } from "react";
 import { Task } from "./Task";
 
-export function TaskList() {
-  return (
+interface TaskListProps {
+  tasksCreated: number;
+  concludedTasks: number;
+}
+
+export function TaskList({ tasksCreated, concludedTasks }: TaskListProps) {
+    return (
     <div className="flex flex-col gap-4 items-center justify-center text-gray-300 mt-16">
       <div className="w-full flex justify-between text-gray-200">
         <div className="flex gap-2 items-center font-bold">
           <h2 className="text-blue">Tarefas criadas</h2>
           <span className="px-2 py-[2px] text-xs bg-gray-400 rounded-full">
-            0
+            {tasksCreated}
           </span>
         </div>
 
         <div className="flex gap-2 items-center font-bold">
           <h2 className="text-purple">Concluídas</h2>
           <span className="px-2 py-[2px] text-xs bg-gray-400 rounded-full">
-            0
+            {concludedTasks} de {tasksCreated}
           </span>
         </div>
       </div>
