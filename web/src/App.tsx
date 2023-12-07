66638e7b05caa3dc4f074df33ee27dccb687dcdc
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-12-07 01:51:17 -0300

    :sparkles: feat: add handleAddTask function

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,46 +1,43 @@
 import { useState } from 'react'
+
+import { Task } from './components/Task'
 import { TaskList } from './components/TaskList'
 
 import Header from './components/Header'
 
-export interface Task {
-  id: string
-  title: string
-  isConcluded: boolean
-}
-
-const predefinedTasks: Task[] = [
-  {
-    id: '1',
-    title: 'Laboris aliquip consectetur cillum laboris proident exercitation. Deserunt exercitation cupidatat officia reprehenderit.',
-    isConcluded: false
-  },
-  {
-    id: '2',
-    title: 'Laboris aliquip consectetur cillum laboris proident exercitation. Deserunt exercitation cupidatat officia reprehenderit.',
-    isConcluded: false
-  },
-  {
-    id: '3',
-    title: 'Laboris aliquip consectetur cillum laboris proident exercitation. Deserunt exercitation cupidatat officia reprehenderit.',
-    isConcluded: false
-  },
-  {
-    id: '4',
-    title: 'Laboris aliquip consectetur cillum laboris proident exercitation. Deserunt exercitation cupidatat officia reprehenderit.',
-    isConcluded: false
-  }
-]
-
 export default function App () {
-  // const [tasks, setTasks] = useState<Task[]>([])
-  const [tasks, setTasks] = useState<Task[]>(predefinedTasks)
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [inputValue, setInputValue] = useState('')
 
   const concludedTasksCounter = tasks.filter(task => task.isConcluded).length
 
+  /**
+   * O (prevTasks) evita que o React sobrescreva o estado anterior. Assim, o
+   * React irá pegar o estado anterior e adicionar o novo estado. Isso torna a
+   * aplicação mais clara e evita bugs.
+   */
+  function handleAddTask () {
+    if (!inputValue) {
+      return
+    }
+
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: inputValue,
+      isConcluded: false
+    }
+
+    setTasks(prevTasks => [...prevTasks, newTask])
+    setInputValue('')
+  }
+
   return (
     <div className='flex flex-col gap-4 items-center justify-center'>
-      <Header />
+      <Header
+        handleAddTask={handleAddTask}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+      />
 
       <main className='w-full max-w-[46rem] flex flex-col gap-16 p-4 sm:p-0'>
         <TaskList
